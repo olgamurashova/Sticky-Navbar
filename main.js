22e9192cee52b98e563a8520ef132aa5b2f10a10
Olga Murashova <om.dreyfus@gmail.com>
2022-09-11 22:45:42 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector(".navbar");
+
+const bottomContainer = document.querySelector(".bottom-container");
+
+console.log(navbar.offsetHeight); //height of navbar
+
+console.log(bottomContainer.offsetTop);
+
+window.addEventListener("scroll", ()=>{
+   if(window.scrollY > bottomContainer.offsetTop - navbar.offsetHeight - 50){
+    navbar.classList.add("active");
+   }
+   else {
+    navbar.classList.remove("active");
+   }
+})
